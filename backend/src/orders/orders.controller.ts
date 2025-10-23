25a1e259458075bc48a18b3fc0d08d12c7155598
danieljurkowski <dnl.jurkowski@gmail.com>
2025-10-23 20:25:54 +0200

    KAN-22: Add jwt auth guards to the endpoints

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -1,18 +1,21 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
+import {JwtAuthGuard} from "../auth/jwt-auth.guard";
 
 @Controller('orders')
 export class OrdersController {
     constructor(private readonly ordersService: OrdersService) {}
 
     // GET /orders - pobiera wszystkie zamówienia
+    @UseGuards(JwtAuthGuard)
     @Get()
     async findAll() {
         return this.ordersService.findAll();
     }
 
     // POST /orders - tworzy nowe zamówienie
+    @UseGuards(JwtAuthGuard)
     @Post()
     async create(@Body() dto: CreateOrderDto) {
         return this.ordersService.create(dto);
