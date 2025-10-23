9c794934ef702ddd72191d39ff337cabd074a989
danieljurkowski <dnl.jurkowski@gmail.com>
2025-10-23 20:18:21 +0200

    KAN-22: Add jwt auth guards to the endpoints

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,16 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '../../prisma/prisma.module';
+import {JwtStrategy} from "./jwt.strategy";
 
 @Module({
     imports: [
         PrismaModule,
         JwtModule.register({
-            secret: process.env.JWT_SECRET || 'SUPER_SECRET_KEY', // w .env
-            signOptions: { expiresIn: '1h' },
+            secret: process.env.JWT_SECRET || 'SUPER_SECRET_KEY',
         }),
     ],
-    providers: [AuthService],
+    providers: [AuthService, JwtStrategy],
     controllers: [AuthController],
+    exports: [AuthService],
 })
 export class AuthModule {}
--- backend/src/colognes/colognes.module.ts
+++ backend/src/colognes/colognes.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ColognesService } from './colognes.service';
 import { ColognesController } from './colognes.controller';
-import {PrismaModule} from "../../prisma/prisma.module";
+import { PrismaModule } from "../../prisma/prisma.module";
 
 @Module({
     imports: [PrismaModule],
