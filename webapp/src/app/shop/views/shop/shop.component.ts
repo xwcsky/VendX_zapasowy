2463c22a191cb0748a6494e6c30edd95c3cf6833
Wiktor Jurkowski <wicio100@interia.pl>
2026-01-13 22:33:52 +0100

    first commit

--- webapp/src/app/shop/views/shop/shop.component.ts
+++ webapp/src/app/shop/views/shop/shop.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core'; import { ColognesListComponent } from
 import { AuthService } from '../../../auth/auth.service'; import { QRCodeComponent } from 'angularx-qrcode';
 import { ConfigurationService } from '../../../common/services/configuration.service';
 import { BreadcrumbsComponent } from '../../../breadcrumbs/breadcrumbs'; 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
    @Component(
     { selector: 'app-shop',
-       imports: [ColognesListComponent, QRCodeComponent, BreadcrumbsComponent],
-        templateUrl: './shop.component.html', styleUrl: './shop.component.scss' }
+      standalone: true,
+      imports: [ColognesListComponent, QRCodeComponent, BreadcrumbsComponent, FormsModule, CommonModule],
+      templateUrl: './shop.component.html', styleUrl: './shop.component.scss' }
         
       ) export class ShopComponent { cologneId: string | undefined; payConfirmed: boolean = false;
          constructor(public authService: AuthService)
@@ -16,10 +20,59 @@ import { BreadcrumbsComponent } from '../../../breadcrumbs/breadcrumbs';
           showPaymentStep = false;
 
           confirmPayment(): void {
-             this.payConfirmed = true;
-             this.showPaymentStep = true;
+            this.resetPaymentState();
+
+            this.payConfirmed = true;
+            this.showPaymentStep = true;
+         
           }
           onBreadcrumbClick(url: string) {
+            this.resetPaymentState();
             this.payConfirmed = false;
-          }          
+          } 
+            discountCode = '';
+            discountPercent = 0;
+            qrReady = false;
+            qrData = '';
+
+            applyDiscount() {
+               if (this.discountCode === 'TEST50') {
+                 this.discountPercent = 50;
+               } else {
+                 this.discountPercent = 0;
+                 alert('Niepoprawny kod rabatowy');
+               }
+               this.generateQr();
+             }
+             
+
+            generateQr() {
+               // najpierw reset starego QR
+               this.qrReady = false;
+               this.qrData = '';
+             
+               const baseUrl = ConfigurationService.getHostUrl() + '/payment';
+               const payload = {
+                 cologneId: this.cologneId,
+                 discount: this.discountPercent
+               };
+             
+               // mały timeout, żeby Angular odświeżył DOM
+               setTimeout(() => {
+                 this.qrData = baseUrl + '?data=' + encodeURIComponent(JSON.stringify(payload));
+                 this.qrReady = true;
+               }, 0);
+             }
+             resetPaymentState() {
+               this.discountCode = '';
+               this.discountPercent = 0;
+               this.qrReady = false;
+               this.qrData = '';
+               this.payConfirmed = false;
+               this.showPaymentStep = false;
+             }
+
+             
+             
+             
            readonly ConfigurationService = ConfigurationService; }
